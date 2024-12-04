0f1397542805bfa3f4997b9e9c506348522bc90d
Tor Steinar <tor.steinar.gulliksen@vestfoldfylke.no>
2024-12-04 13:09:39 +0100

    La til ejs som view engine

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const db = require(`./db`); // Databasetilkobling
 // Globale variabler
 const PORT = 8000;
 
+// Sette ejs som view engine
+app.set(`view engine`, `ejs`);
+
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
